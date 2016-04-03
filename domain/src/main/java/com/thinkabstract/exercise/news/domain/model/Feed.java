e7b9fb35aae9d8f7304f7b2a4574ae0fd7c16791
Gabor.Peto <gabor.mask.peto@gmail.com>
2016-04-03 13:23:43 +0100

    Implement toString, equals and hashCode methods

--- domain/src/main/java/com/thinkabstract/exercise/news/domain/model/Feed.java
+++ domain/src/main/java/com/thinkabstract/exercise/news/domain/model/Feed.java
@@ -9,4 +9,38 @@ public class Feed {
   public String description;
 
   public List<FeedEntry> entries;
+
+  @Override public String toString() {
+    return "Feed{" +
+        "title='" + title + '\'' +
+        ", description='" + description + '\'' +
+        ", entries=" + entries +
+        '}';
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Feed feed = (Feed) o;
+
+    if (title != null ? !title.equals(feed.title) : feed.title != null) {
+      return false;
+    }
+    if (description != null ? !description.equals(feed.description) : feed.description != null) {
+      return false;
+    }
+    return entries != null ? entries.equals(feed.entries) : feed.entries == null;
+  }
+
+  @Override public int hashCode() {
+    int result = title != null ? title.hashCode() : 0;
+    result = 31 * result + (description != null ? description.hashCode() : 0);
+    result = 31 * result + (entries != null ? entries.hashCode() : 0);
+    return result;
+  }
 }
--- domain/src/main/java/com/thinkabstract/exercise/news/domain/model/FeedEntry.java
+++ domain/src/main/java/com/thinkabstract/exercise/news/domain/model/FeedEntry.java
@@ -11,4 +11,47 @@ public class FeedEntry {
   public String content;
 
   public Date publishedDate;
+
+  @Override public String toString() {
+    return "FeedEntry{" +
+        "title='" + title + '\'' +
+        ", contentSnipet='" + contentSnipet + '\'' +
+        ", content='" + content + '\'' +
+        ", publishedDate=" + publishedDate +
+        '}';
+  }
+
+  @Override public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    FeedEntry feedEntry = (FeedEntry) o;
+
+    if (title != null ? !title.equals(feedEntry.title) : feedEntry.title != null) {
+      return false;
+    }
+    if (contentSnipet != null
+        ? !contentSnipet.equals(feedEntry.contentSnipet)
+        : feedEntry.contentSnipet != null) {
+      return false;
+    }
+    if (content != null ? !content.equals(feedEntry.content) : feedEntry.content != null) {
+      return false;
+    }
+    return publishedDate != null
+        ? publishedDate.equals(feedEntry.publishedDate)
+        : feedEntry.publishedDate == null;
+  }
+
+  @Override public int hashCode() {
+    int result = title != null ? title.hashCode() : 0;
+    result = 31 * result + (contentSnipet != null ? contentSnipet.hashCode() : 0);
+    result = 31 * result + (content != null ? content.hashCode() : 0);
+    result = 31 * result + (publishedDate != null ? publishedDate.hashCode() : 0);
+    return result;
+  }
 }
